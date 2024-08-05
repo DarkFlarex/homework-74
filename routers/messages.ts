b3e3acb5920fc8101c7b463ddb0de94ce8f4ebaa
DarkFlarex <darkflarexxx@gmail.com>
2024-08-06 01:48:28 +0600

    Сделал get запрос

--- routers/messages.ts
+++ routers/messages.ts
@@ -4,7 +4,21 @@ import { IMessage } from '../types';
 
 const messagesRouter = express.Router();
 
+const data: IMessage[] = [];
 const messagesDir = './messages';
+
+messagesRouter.get('/', async (req, res) => {
+    const files = await fs.readdir(messagesDir);
+    files.reverse();
+    const latestFiles = files.slice(0, 5);
+    for (const file of latestFiles) {
+        const path = `${messagesDir}/${file}`;
+        const fileContents = await fs.readFile(path);
+        const message: IMessage = JSON.parse(fileContents.toString());
+        data.push(message);
+    }
+    res.send(data);
+});
 
 const fileName = () => {
     const datetime = new Date().toISOString().replace(/:/g, '-').replace(/\./g, '-');
